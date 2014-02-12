2512c623ae09ec7f84f7037331c03c6bf810ffe9
Benjamin W. Smith <benjaminwarfield@just-another.net>
2014-02-12 15:34:06 -0500

    Prep fixes
    - Be smarted with generating requirements
    - Use setuptools setup, distutils commands

--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
-from distutils.core import setup
 import os
+from setuptools import setup
+from distutils.command.sdist import sdist
+from distutils.command.bdist import bdist
+
+from pip.req import parse_requirements
 
 cwd = os.path.abspath(os.path.dirname(__file__))
-requirements_txt = filter(None, open(os.path.join(cwd, 'requirements.txt')).read().split('\n'))
+
+requirements_txt = parse_requirements(os.path.join(cwd, 'requirements.txt'))
+requirements = [str(r.req) for r in requirements_txt]
 readme = open(os.path.join(cwd, 'README.md')).read()
 
 setup(name='pypi-cdn-log-archiver',
-      version='0.1.1',
+      version='0.1.3',
       description='log archiver for pypi cdn logs',
       long_description=readme,
       author='Benjamin W. Smith',
       author_email='benjaminwarfield@gmail.com',
       packages=[],
       url='http://github.com/benjaminws/pypi-cdn-log-archiver',
-      install_requires=requirements_txt,
+      install_requires=requirements,
       scripts=['src/bin/pypi-cdn-log-archiver'],
       platforms = 'Posix; MacOS X; Windows',
 ) 
