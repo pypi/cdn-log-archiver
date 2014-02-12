35fdf97ecf792d22bacc7981ffd8e85673a8a36d
Benjamin W. Smith <benjaminwarfield@just-another.net>
2014-02-12 10:59:23 -0500

    Improvements & fixes.
    - Implement optional date from env, default to yesterday
    - Bail if logs aren't found to compress
    - Only remove old logs if compression and archiving worked
    - Update README

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # PyPI CDN Log Archiver
 
-1. Get yesterdays raw logs from DreamObjects, written by the cdn
+1. Get raw logs from DreamObjects, written by the cdn
 2. Archive and compress them
-3. Upload archive back to DreamObjects in a same structure
-4. Remove raw logs
+3. Upload archive back to DreamObjects in a sane structure
+4. Remove raw logs that were archived
 
 # Usage
 
@@ -13,7 +13,14 @@ You'll need to export some env variables to get started:
     $ export DHO_SECRET_KEY=<your_access_key>
     $ export PYPI_LOG_BUCKET=<bucket_name>
 
-Now run it:
+Optionally you can set the date to archive like so:
+
+    $ export PYPI_LOG_DATE=2014-02-11
+
+If you don't set `PYPI_LOG_DATE`, the script will default to looking for
+yesterdays date.
+
+Now you should be able to run it:
 
     $ python pypi-cdn-log-archiver.py
     112373 bytes written to testpypi-cdn-logs/archive/2014/2
--- pypi-cdn-log-archiver.py
+++ pypi-cdn-log-archiver.py
@@ -8,7 +8,7 @@ import tarfile
 import boto
 import boto.s3.connection
 from contextlib import contextmanager
-from datetime import date
+from datetime import datetime, timedelta
 
 
 @contextmanager
@@ -57,10 +57,17 @@ def main():
     access_key = os.environ['DHO_ACCESS_KEY']
     secret_key = os.environ['DHO_SECRET_KEY']
     bucket_name = os.environ['PYPI_LOG_BUCKET']
-    todays_date = str(date.today())
-    look_for = 'incoming/{0}'.format(todays_date)
-    save_to = 'archive/{0}/{1}'.format(date.today().year,
-                                       date.today().month)
+
+    # Look for the date to archive in the env, default to yesterday
+    if os.environ.get('PYPI_LOG_DATE', None):
+        target_date = datetime.strptime(os.environ.get('PYPI_LOG_DATE'),
+                                        '%Y-%m-%d')
+    else:
+        target_date = (datetime.today() - timedelta(days=1))
+
+    look_for = 'incoming/{0}'.format(target_date.strftime('%Y-%m-%d'))
+    save_to = 'archive/{0}/{1}'.format(target_date.year,
+                                       target_date.month)
 
     conn = boto.connect_s3(
         aws_access_key_id=access_key,
@@ -70,14 +77,20 @@ def main():
     )
 
     bucket = conn.get_bucket(bucket_name)
-
     todays_logs = collect_todays_logs(bucket, look_for)
-    bytes_written = archive_logs(bucket, todays_logs, save_to, todays_date)
-    remove_raw_logs(bucket, todays_logs)
+
+    if len(todays_logs) == 0:
+        print('No logs found at {0}'.format(look_for))
+        return 1
+
+    bytes_written = archive_logs(bucket, todays_logs, save_to,
+            target_date.strftime('%Y-%m-%d'))
 
     if bytes_written and bytes_written > 0:
         print('{0} bytes written to {1}/{2}'.format(bytes_written,
                                                     bucket_name, save_to))
+
+        remove_raw_logs(bucket, todays_logs)
         return 0
     else:
         print('Nothing written to {0}/{1}'.format(bucket_name, save_to))
